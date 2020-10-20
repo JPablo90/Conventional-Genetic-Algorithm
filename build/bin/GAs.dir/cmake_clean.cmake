22e5592cedd721556321391a69d228ff310e519b
JPablo90 <jp.garciamurillo@gmail.com>
2020-10-20 14:29:38 -0500

    Project settings
    In this step is created the cmake configuration and tree
    directories for the genetic algorithm project.
    
    -------------------------------------------------------

---
+++ build/bin/GAs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/./../bin/GAs.dir/src/main.cpp.o"
+  "./../bin/GAs.pdb"
+  "./../bin/GAs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/./../bin/GAs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
